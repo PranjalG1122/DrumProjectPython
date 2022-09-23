88b7b6f6973ee57dbe3de103726469d5133d9a70
Pranjal Gupta <pranjalg1122@gmail.com>
2022-09-23 12:06:07 +0530

    Added draw grid

--- drums.py
+++ drums.py
@@ -8,15 +8,48 @@ HEIGHT = 800
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (128, 128, 128)
+dark_gray = (64, 64, 64)
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Beats Maker")
-label_font = pygame.font.Font("Robot-Bold.ttf", 32)
+label_font = pygame.font.Font("Roboto-Bold.ttf", 32)
 
 fps = 60
 timer = pygame.time.Clock()
+instruments = 6
+
+
+def draw_grid():
+    left_menu = pygame.draw.rect(screen, gray, [0, 0, 250, HEIGHT - 200], 5)
+    bottom_menu = pygame.draw.rect(
+        screen, dark_gray, [0, HEIGHT-200, WIDTH, 200], 5)
+    hi_hat_text = label_font.render("Hi-Hat", True, white)
+    screen.blit(hi_hat_text, (30, 30))
+    snare_text = label_font.render("Snare", True, white)
+    screen.blit(snare_text, (30, 130))
+    kick_text = label_font.render("Kick", True, white)
+    screen.blit(kick_text, (30, 230))
+    crash_text = label_font.render("Crash", True, white)
+    screen.blit(crash_text, (30, 330))
+    clap_text = label_font.render("Clap", True, white)
+    screen.blit(clap_text, (30, 430))
+    floor_tom_text = label_font.render("Floor Tom", True, white)
+    screen.blit(floor_tom_text, (30, 530))
+
+    for i in range(instruments):
+        pygame.draw.line(screen, gray, (0, (i * 100 + 100)),
+                         (250, (i * 100 + 100)), 5)
+
 
 run = True
 while run:
     timer.tick(fps)
     screen.fill(black)
+    draw_grid()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+
+pygame.quit()
